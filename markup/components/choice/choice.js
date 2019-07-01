3184ab8e14b70d37be51d8b3366e2990ba82fd08
Bpemena <vakpaevk@mail.ru>
2019-07-01 17:26:08 +0600

    first commit

---
+++ markup/components/choice/choice.js
@@ -0,0 +1,8 @@
+ var swiper = new Swiper('.choice__container', {
+      slidesPerView: 'auto',
+      loop: true,
+      loopedSlides: 3,
+      spaceBetween: 50,
+      freeMode: true,
+      freeModeMomentum: true,
+    });
---
+++ markup/components/sub/sub.js
@@ -0,0 +1,8 @@
+ var swiper = new Swiper('.sub__container', {
+      slidesPerView: 'auto',
+      loop: true,
+      loopedSlides: 3,
+      spaceBetween: 9,
+      freeMode: true,
+      freeModeMomentum: true,
+    });
