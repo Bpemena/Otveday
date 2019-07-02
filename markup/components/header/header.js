ced1524f0383d4e2ead026a531513005b37c8a5b
Bpemena <vakpaevk@mail.ru>
2019-07-02 18:46:07 +0600

    5 commit

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,3 +1,4 @@
+var gg;
 $(document).ready(function() {
 	// ======================================================
 	// Открытие закрытие попапов
@@ -38,44 +39,50 @@ $(document).ready(function() {
 	});
 	// ======================================================
 	// Арифметика со списком
-	function summa() {
-		var sum =0,a=0;
-		$('.spisok__item').each(function() {
-			a = a + $(this).data('price');
-			sum = sum + a;
-			console.log(sum);
-		})
+	gg = function summa() {
+		var a=0, b=0;
+		$('.spisok__item-d').each(function() {
+			a = a + ($(this).find('.spisok__number').val() * Number($(this).data('price')));
+		});
+		$('.spisok__item-m').each(function() {
+			b = b + ($(this).find('.spisok__number').val() * Number($(this).data('price')));
+		});
+		$('.basketpopup__oplata-d').html('Оплатить '+a+'₽')
+		$('.basketpopup__oplata-m').html('Оплатить '+b+'₽')
+		$('.basketpopup__itog-sum-d').html(a+'₽')
+		$('.basketpopup__itog-sum-m').html(b+'₽')
+		console.log(a);
+		console.log(b);
 	}
-	// function summa(price) {
-	// 	var sum = 0;
-	// 	sum = sum + price;
-	// 	$('.basketpopup__oplata').html('Оплатить '+ sum +'₽').attr('data-sum',sum);
-	// 	$('.basketpopup__itog-sum').html(sum +'₽');
-	// }
-	// var minus = 0, plus = 0, sum = 0;
+	
 	$('.dobavka').click(function(e) {
 		e.preventDefault();
 		$('.tovar').slideToggle().delay(1000).slideToggle();
 		$('.mobiletovar').slideToggle().delay(1000).slideToggle();
-		$('.spisok').append('<div class="spisok__item" data-price="'+$(this).data('price')+'"><div class="spisok__img"><img src="'+$(this).data('img')+'"></div><div class="spisok__info"><div class="spisok__topline"><div class="spisok__name">'+$(this).data('name')+'</div><div class="spisok__price">'+$(this).data('price')+'₽</div></div><div class="spisok__buttons"><div class="spisok__minus">-</div><input type="number" min="1" max="100" class="spisok__number" value="1"><div class="spisok__plus">+</div><div class="spisok__del"><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.3 0.709971C12.91 0.319971 12.28 0.319971 11.89 0.709971L6.99997 5.58997L2.10997 0.699971C1.71997 0.309971 1.08997 0.309971 0.699971 0.699971C0.309971 1.08997 0.309971 1.71997 0.699971 2.10997L5.58997 6.99997L0.699971 11.89C0.309971 12.28 0.309971 12.91 0.699971 13.3C1.08997 13.69 1.71997 13.69 2.10997 13.3L6.99997 8.40997L11.89 13.3C12.28 13.69 12.91 13.69 13.3 13.3C13.69 12.91 13.69 12.28 13.3 11.89L8.40997 6.99997L13.3 2.10997C13.68 1.72997 13.68 1.08997 13.3 0.709971Z" fill="#BDBDBD"/></svg></div></div></div></div>')
+		$('.spisok-d').append('<div class="spisok__item spisok__item-d" data-price="'+$(this).data('price')+'"><div class="spisok__img"><img src="'+$(this).data('img')+'"></div><div class="spisok__info"><div class="spisok__topline"><div class="spisok__name">'+$(this).data('name')+'</div><div class="spisok__price">'+$(this).data('price')+'₽</div></div><div class="spisok__buttons"><div class="spisok__minus">-</div><input type="number" min="1" max="100" class="spisok__number" value="1"><div class="spisok__plus">+</div><div class="spisok__del"><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.3 0.709971C12.91 0.319971 12.28 0.319971 11.89 0.709971L6.99997 5.58997L2.10997 0.699971C1.71997 0.309971 1.08997 0.309971 0.699971 0.699971C0.309971 1.08997 0.309971 1.71997 0.699971 2.10997L5.58997 6.99997L0.699971 11.89C0.309971 12.28 0.309971 12.91 0.699971 13.3C1.08997 13.69 1.71997 13.69 2.10997 13.3L6.99997 8.40997L11.89 13.3C12.28 13.69 12.91 13.69 13.3 13.3C13.69 12.91 13.69 12.28 13.3 11.89L8.40997 6.99997L13.3 2.10997C13.68 1.72997 13.68 1.08997 13.3 0.709971Z" fill="#BDBDBD"/></svg></div></div></div></div>')
+		$('.spisok-m').append('<div class="spisok__item spisok__item-m" data-price="'+$(this).data('price')+'"><div class="spisok__img"><img src="'+$(this).data('img')+'"></div><div class="spisok__info"><div class="spisok__topline"><div class="spisok__name">'+$(this).data('name')+'</div><div class="spisok__price">'+$(this).data('price')+'₽</div></div><div class="spisok__buttons"><div class="spisok__minus">-</div><input type="number" min="1" max="100" class="spisok__number" value="1"><div class="spisok__plus">+</div><div class="spisok__del"><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.3 0.709971C12.91 0.319971 12.28 0.319971 11.89 0.709971L6.99997 5.58997L2.10997 0.699971C1.71997 0.309971 1.08997 0.309971 0.699971 0.699971C0.309971 1.08997 0.309971 1.71997 0.699971 2.10997L5.58997 6.99997L0.699971 11.89C0.309971 12.28 0.309971 12.91 0.699971 13.3C1.08997 13.69 1.71997 13.69 2.10997 13.3L6.99997 8.40997L11.89 13.3C12.28 13.69 12.91 13.69 13.3 13.3C13.69 12.91 13.69 12.28 13.3 11.89L8.40997 6.99997L13.3 2.10997C13.68 1.72997 13.68 1.08997 13.3 0.709971Z" fill="#BDBDBD"/></svg></div></div></div></div>')
+		gg();	
 	});
 	
 	$('body').on('click','.spisok__del',function(e) {
 		e.preventDefault();
 		$(this).parent().parent().parent().remove();
-		$('.basketpopup__oplata').html('Оплатить '+ sum +'₽');
-		$('.basketpopup__itog-sum').html(sum +'₽');
+		gg();
 	});
 
 	$('body').on('click','.spisok__minus', function(e) {
 		e.preventDefault();
-		minus = $(this).parent().find('.spisok__number').val() - 1;
-		$(this).parent().find('.spisok__number').val(minus); 
+		if ($(this).parent().find('.spisok__number').val() > 1) {
+			minus = $(this).parent().find('.spisok__number').val() - 1;
+			$(this).parent().find('.spisok__number').val(minus);
+			gg(); 
+		}
 	});
 	$('body').on('click','.spisok__plus', function(e) {
 		e.preventDefault();
 		plus = parseInt($(this).parent().find('.spisok__number').val()) + 1;
 		$(this).parent().find('.spisok__number').val(plus);
+		gg();
 	});
 	
 	// ======================================================
